a728b0851d1c0066fa299f083535e884301bbb19
Guilherme <111528193+guihi1@users.noreply.github.com>
2025-01-17 17:02:00 -0300

    Install necessary packages and adjust index.js accordingly

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import Express from 'express';
-import router from './routes/route.js';
+import router from './routes/router.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import session from 'express-session';
 
 const app = Express();
 const port = 3000;
 
 // view engine setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
